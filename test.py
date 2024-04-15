68143a22ba42500a73b7810beb1ee82f5ada1d23
Michał Nowakowski <340757@uwr.edu.pl>
2024-04-16 00:31:44 +0200

    Possible read of real analog input.

--- test.py
+++ test.py
@@ -1,19 +1,37 @@
 from flask import Flask, render_template, Response
 import time
-import Adafruit_ADS1x15
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 app = Flask(__name__)
 
-# Initialize ADC
-adc = Adafruit_ADS1x15.ADS1115()
-GAIN = 1
+# Initializing I2C
+i2c = busio.I2C(board.SCL, board.SDA)
 
-# Route to main page.
+# Setting data rate
+ads_data_rate = 250
+
+# Initializing ADS1115
+ads = ADS.ADS1115(i2c, data_rate=ads_data_rate)
+
+# Setting measure channel ADC (AI1)
+chan = AnalogIn(ads, ADS.P1)
+
+# Frequency of reading data by app
+frequency = (1/data_rate)
+
+# Constant value to convert data from bites to volts
+VOLTAGE_REFERENCE = 4.096  # For ADS1115 using GAIN=1
+
+# Empty list for storing data.
+data_points = []
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route to graph page.
 @app.route('/graph')
 def graph():
     return render_template('graph.html')
@@ -22,16 +40,26 @@ def graph():
 def data():
     def generate():
         while True:
-            # Read data from ADC channel 1 (AI1)
-            value = adc.read_adc(1, gain=GAIN)
-            
-            # Convert data to proper format
-            data_string = f"data: {value}\n\n"
+            # Reading raw value from ADC
+            raw_value = chan.value
+
+            # Converting data to volts
+            voltage = (raw_value / 65535) * VOLTAGE_REFERENCE  # 65535 is max bit value of ADC data
+
+            # Adding data to list
+            data_points.append(voltage)
+
+            if len(data_points) > 100:
+                # If list contain 100 points, delete last one, to leave it always with 100 only
+                data_points.pop(0)
+
+            # Converting data to proper format for app
+            data_string = f"data: {','.join(map(str, data_points))}\n\n"
 
             yield data_string
-            time.sleep(0.1)  # Adjust frequency of data sending
+            time.sleep(frequency)
 
     return Response(generate(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host='0.0.0.0', port=80)
