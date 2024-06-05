f21b21ce32de1f6956028812edeba664b82b60ee
Michał Nowakowski <340757@uwr.edu.pl>
2024-06-05 10:51:19 +0200

    Final app.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, session
+from flask import Flask, render_template, Response, session, jsonify, request
 import time
 import board
 import busio
@@ -10,64 +10,81 @@ app.secret_key = 'test'
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
-ads_data_rate = 250
-ads = ADS.ADS1115(i2c, data_rate=ads_data_rate)
+ads_data_rate = 860
+ads = ADS.ADS1115(i2c, data_rate=ads_data_rate, gain=1)
 chan = AnalogIn(ads, ADS.P1)
+freq = 10
+period = (1 / (ads_data_rate / (freq / 5)))
 
+is_generating = True
+VOLTAGE_REFERENCE = 4.096
 
-
-frequency = (1/ads_data_rate)
-reading_pause = False
-# Constant value to convert data from bites to volts
-VOLTAGE_REFERENCE = 4.096  # For ADS1115 using GAIN=1
-# ALSO CAN CHANGE GAIN, OR ADD SLIDER FOR IT, IDK IF THIS IS THE SAME AS GAIN
-    
-# Empty list for storing data.
 data_points = []
+graph_points_amount = 102
+
 
 def read_session_value(obj: str, default: bool):
     with app.test_request_context():
         return session.get(obj, default)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/graph')
 def graph():
     return render_template('graph.html')
 
+
 @app.route('/data')
 def data():
     def generate():
-        while not reading_pause:
-            # Reading raw value from ADC
+        while is_generating:
             raw_value = chan.value
-
-            # Converting data to volts
-            voltage = (raw_value / 65535) * VOLTAGE_REFERENCE  # 65535 is max bit value of ADC data
-
-            # Adding data to list
+            voltage = (raw_value / 65535) * 8
             data_points.append(voltage)
 
-            if len(data_points) > 100:
-                # If list contain 100 points, delete last one, to leave it always with 100 only
+            if len(data_points) > graph_points_amount:
                 data_points.pop(0)
 
-            # Converting data to proper format for app
             data_string = f"data: {','.join(map(str, data_points))}\n\n"
-
             yield data_string
-            time.sleep(frequency)
+            time.sleep(period)
 
     return Response(generate(), mimetype='text/event-stream')
 
+
 @app.route('/toggle_flag', methods=['POST'])
 def toggle_flag():
-    session['flag'] = not session.get('flag', False)
-    global reading_pause
-    reading_pause = session['flag']
-    return Response('')
+    global is_generating
+    is_generating = not is_generating
+    return jsonify(success=True)
+
+
+@app.route('/get_flag_status', methods=['GET'])
+def get_flag_status():
+    return jsonify(isGenerating=is_generating)
+
+
+@app.route('/sampling-period')
+def get_sampling_period():
+    data = {
+        'sampling_period': period,
+        'points_amount': graph_points_amount
+    }
+    return jsonify(data)
+
+
+@app.route('/set_frequency', methods=['POST'])
+def set_frequency():
+    data = request.get_json()
+    session['frequency'] = float(data['frequency'])
+    global period
+    period = 1 / (ads_data_rate / (session['frequency'] / 5))
+    return jsonify({'status': 'success', 'frequency': session['frequency']})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
--- static/js/graph.js
+++ static/js/graph.js
@@ -1,64 +1,133 @@
 var eventSource = new EventSource('/data');
-var graph_voltage_range = 5; //has to be set to default voltage scale input value
-var graph_dc_offset = 0; //has to be set to default dc offset input value
+var graph_voltage_range = 5; // has to be set to default voltage scale input value
+var graph_dc_offset = 0; // has to be set to default dc offset input value
 
-function updateButtonText(flag) {
+var isGenerating = true;
+
+function updateButtonText() {
     var button = document.getElementById("toggleButton");
-    button.textContent = flag ? "Stop" : "Start";
+    button.textContent = isGenerating ? "Stop" : "Start";
 }
 
 function toggleFlag() {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/toggle_flag", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    // xhr.onload = function () {
-    //     if (xhr.status === 200) {
-    //         var flag = JSON.parse(xhr.responseText).flag;
-    //         updateButtonText(flag);
-    //     }
-    // };
-    
-    xhr.send();
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            isGenerating = !isGenerating;
+            updateButtonText();
+        }
+    };
+    xhr.send(JSON.stringify({ toggle: true }));
 }
 
-eventSource.onmessage = function(event) {
-    var data = event.data.split(',');
-    var x = data.slice(0, data.length / 2);
-    var y = data.slice(data.length / 2);
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("/get_flag_status")
+        .then(response => response.json())
+        .then(data => {
+            isGenerating = data.isGenerating;
+            updateButtonText();
+        })
+        .catch(error => console.error("Error fetching flag status:", error));
+});
 
-    var ctx = document.getElementById('chartContainer').getContext('2d');
-    
-    document.getElementById('voltageRange').addEventListener('input', function() { graph_voltage_range = parseFloat(this.value);});
-    document.getElementById('dcOffset').addEventListener('input', function() { graph_dc_offset = parseFloat(this.value);});
+function setSamplingFreq(value) {
+    document.getElementById('samplingFreqValue').innerText = value;
+        fetch('/set_frequency', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ frequency: value })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        initializeChart();
+    })
+    .catch((error) => console.error('Error:', error));
+}
 
-    if (!window.chart) {
-        window.chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: x,
-                datasets: [{
-                    label: 'Data',
-                    data: y,
-                    borderWidth: 1,
-                    borderColor: 'red'
-                }]
-            },
-            options: {
-                responsive: false,
-                scales: {
-                    y: {
-                        min: ((graph_voltage_range * (-1)) + graph_dc_offset),
-                        max: (graph_voltage_range + graph_dc_offset)
+function getTimeLabel(callback) {
+    fetch('/sampling-period')
+    .then(response => response.json())
+    .then(data => {
+        const samplingPeriod = data.sampling_period;
+        const totalPoints = data.points_amount;
+
+        const labels = [];
+        const timeStep = 1 / (samplingPeriod / 2);
+
+        for (let i = 0; i < totalPoints; i++) {
+            const time = i / timeStep;
+            labels.push(time.toFixed(3));
+        }
+        callback(labels);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        callback([]);
+    });
+}
+
+function initializeChart() {
+    getTimeLabel(function(label) {
+        eventSource.onmessage = function(event) {
+            var data = event.data.split(',');
+            var x = label;
+            var y = data;
+
+            var ctx = document.getElementById('chartContainer').getContext('2d');
+            
+            document.getElementById('voltageRange').addEventListener('input', function() { graph_voltage_range = parseFloat(this.value); });
+            document.getElementById('dcOffset').addEventListener('input', function() { graph_dc_offset = parseFloat(this.value); });
+
+            if (!window.chart) {
+                window.chart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: x,
+                        datasets: [{
+                            label: 'Voltage [V]',
+                            data: y,
+                            borderWidth: 1,
+                            borderColor: 'red'
+                        }]
+                    },
+                    options: {
+                        responsive: false,
+                        scales: {
+                            x: {
+                                title: {
+                                    display: true,
+                                    text: 'Time [s]'
+                                }
+                            },
+                            y: {
+                                min: ((graph_voltage_range * (-1)) + graph_dc_offset),
+                                max: (graph_voltage_range + graph_dc_offset)
+                            }
+                        },
+                        animation: {
+                            duration: 0
+                        },
+                        hover: {
+                            animationDuration: 0
+                        },
+                        responsiveAnimationDuration: 0
                     }
-                }
+                });
+            } else {
+                window.chart.data.labels = x;
+                window.chart.data.datasets[0].data = y;
+                window.chart.options.scales.y.min = ((graph_voltage_range * (-1)) + graph_dc_offset);
+                window.chart.options.scales.y.max = (graph_voltage_range + graph_dc_offset);
+                window.chart.update();
             }
-        });
-    } else {
-        window.chart.data.labels = x;
-        window.chart.data.datasets[0].data = y;
-        window.chart.options.scales.y.min = ((graph_voltage_range * (-1)) + graph_dc_offset);
-        window.chart.options.scales.y.max = (graph_voltage_range + graph_dc_offset);
-        window.chart.update();
-    }
-};
+        };
+    });
+}
+
+initializeChart();
+updateButtonText();
